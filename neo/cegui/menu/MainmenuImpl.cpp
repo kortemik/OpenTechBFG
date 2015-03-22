2b06b1ed68502eebcefab43123daefeb65379605
Motoko Kusanagi <kordex@mail.xnet.fi>
2015-03-22 10:32:33 +0200

    and astyle. i really need a precommit hook

--- neo/cegui/menu/MainmenuImpl.cpp
+++ neo/cegui/menu/MainmenuImpl.cpp
@@ -4,109 +4,110 @@
 
 #include "../framework/CmdSystem.h"
 
-namespace CEGUIMenu {
-
-	MainmenuImpl::MainmenuImpl()
+namespace CEGUIMenu
 {
-		CreateCEGUIWindow();
-		RegisterHandlers();
-		setVisible(true);
+
+MainmenuImpl::MainmenuImpl()
+{
+	CreateCEGUIWindow();
+	RegisterHandlers();
+	setVisible( true );
 }
 
 
-	MainmenuImpl::~MainmenuImpl()
+MainmenuImpl::~MainmenuImpl()
 {
-		// TODO should remove subscriptions and windows
+	// TODO should remove subscriptions and windows
 }
 
-	void MainmenuImpl::CreateCEGUIWindow()
+void MainmenuImpl::CreateCEGUIWindow()
+{
+	if( idCEGUI::IsInitialized() )
 	{
-		if (idCEGUI::IsInitialized())
+		CEGUI::System::getSingleton().getDefaultGUIContext()
+		.getRootWindow()->addChild(
+			CEGUI::WindowManager::getSingleton().
+			loadLayoutFromFile( "mainmenu.layout" ) );
+	}
+}
+
+void MainmenuImpl::RegisterHandlers()
+{
+	if( idCEGUI::IsInitialized() )
+	{
+		CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
+									 .getRootWindow()->getChild( "MainMenu" );
+									 
+									 
+		/*
+		* handles mouse on submit to input
+		*/
+		MainmenuWin->getChild( "DemoMapClickArea" )->subscribeEvent(
+			CEGUI::PushButton::EventClicked,
+			&MainmenuImpl::Handle_DemoMapClick,
+			( this )
+		);
+		/*
+		* handles keypress (enter) to input
+		*/
+		
+		// FIXME catches tab too, some way to avoid that?
+		MainmenuWin->getChild( "QuitClickArea" )->subscribeEvent(
+			CEGUI::Combobox::EventTextAccepted,
+			&MainmenuImpl::Handle_QuitClick,
+			( this )
+		);
+	}
+}
+
+bool MainmenuImpl::Handle_DemoMapClick( const CEGUI::EventArgs& )
+{
+	if( idCEGUI::IsInitialized() )
+	{
+		cmdSystem->BufferCommandText( CMD_EXEC_APPEND, "map demo.map\n" );
+		return true;
+	}
+	return false;
+}
+
+bool MainmenuImpl::Handle_QuitClick( const CEGUI::EventArgs& )
+{
+	if( idCEGUI::IsInitialized() )
+	{
+		cmdSystem->BufferCommandText( CMD_EXEC_APPEND, "quit\n" );
+		return true;
+	}
+	return false;
+}
+
+void MainmenuImpl::setVisible( bool visible )
+{
+	if( idCEGUI::IsInitialized() )
+	{
+		CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
+									 .getRootWindow()->getChild( "MainMenu" );
+		MainmenuWin->setVisible( visible );
+		
+		if( visible )
 		{
-			CEGUI::System::getSingleton().getDefaultGUIContext()
-				.getRootWindow()->addChild(
-				CEGUI::WindowManager::getSingleton().
-				loadLayoutFromFile("mainmenu.layout"));
+			CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().show();
+		}
+		else
+		{
+			CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().hide();
 		}
 	}
+}
 
-	void MainmenuImpl::RegisterHandlers()
+bool MainmenuImpl::isVisible()
+{
+	if( idCEGUI::IsInitialized() )
 	{
-		if (idCEGUI::IsInitialized())
-		{
-			CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
-				.getRootWindow()->getChild("MainMenu");
-
-
-			/*
-			* handles mouse on submit to input
-			*/
-			MainmenuWin->getChild("DemoMapClickArea")->subscribeEvent(
-				CEGUI::PushButton::EventClicked,
-				&MainmenuImpl::Handle_DemoMapClick,
-				(this)
-				);
-			/*
-			* handles keypress (enter) to input
-			*/
-
-			// FIXME catches tab too, some way to avoid that?
-			MainmenuWin->getChild("QuitClickArea")->subscribeEvent(
-				CEGUI::Combobox::EventTextAccepted,
-				&MainmenuImpl::Handle_QuitClick,
-				(this)
-				);
-		}
+		CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
+									 .getRootWindow()->getChild( "MainMenu" );
+		return MainmenuWin->isVisible();
 	}
-
-	bool MainmenuImpl::Handle_DemoMapClick(const CEGUI::EventArgs&) 
-	{
-		if (idCEGUI::IsInitialized())
-		{
-			cmdSystem->BufferCommandText(CMD_EXEC_APPEND, "map demo.map\n");
-			return true;
-		}
-		return false;
-	}
-	
-	bool MainmenuImpl::Handle_QuitClick(const CEGUI::EventArgs&) 
-	{
-		if (idCEGUI::IsInitialized())
-		{
-			cmdSystem->BufferCommandText(CMD_EXEC_APPEND, "quit\n");
-			return true;
-		}
-		return false;
-	}
-
-	void MainmenuImpl::setVisible(bool visible)
-	{
-		if (idCEGUI::IsInitialized())
-		{
-			CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
-				.getRootWindow()->getChild("MainMenu");
-			MainmenuWin->setVisible(visible);
-
-			if (visible)
-			{
-				CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().show();
-			}
-			else
-			{
-				CEGUI::System::getSingleton().getDefaultGUIContext().getMouseCursor().hide();
-			}
-		}
-	}
-
-	bool MainmenuImpl::isVisible()
-	{
-		if (idCEGUI::IsInitialized())
-		{
-			CEGUI::Window* MainmenuWin = CEGUI::System::getSingleton().getDefaultGUIContext()
-				.getRootWindow()->getChild("MainMenu");
-			return MainmenuWin->isVisible();
-		}
-		return false;
-	}
+	return false;
+}
 
 } /* namespace CEGUIMenu */
--- neo/cegui/menu/MainmenuImpl.h
+++ neo/cegui/menu/MainmenuImpl.h
@@ -5,23 +5,24 @@
 
 #include "cegui/wrap_cegui.h"
 
-namespace CEGUIMenu {
+namespace CEGUIMenu
+{
 
-	class MainmenuImpl
+class MainmenuImpl
 {
 public:
 	MainmenuImpl();
 	virtual ~MainmenuImpl();
 	
-	void setVisible(bool visible);
+	void setVisible( bool visible );
 	bool isVisible();
-
+	
 private:
 	void CreateCEGUIWindow();
 	void RegisterHandlers();
-	bool Handle_DemoMapClick(const CEGUI::EventArgs&);
-	bool Handle_QuitClick(const CEGUI::EventArgs&);
-
+	bool Handle_DemoMapClick( const CEGUI::EventArgs& );
+	bool Handle_QuitClick( const CEGUI::EventArgs& );
+	
 };
 
 
