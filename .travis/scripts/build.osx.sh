56166d8f4f7ae12401621cb7b07dc368bbc8fa88
Motoko Kusanagi <kordex@gmail.com>
2015-10-11 09:56:40 +0300

    osx needs glew magic

---
+++ .travis/scripts/build.osx.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export SDL_INCLUDE_DIR=/usr/local/opt/sdl/include
+export SDL_LIBRARY=/usr/local/opt/sdl/lib
+
+if [ "$CXX" = "g++" ]; then cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=OFF -DBUNDLED_FREETYPE=ON ../; fi
+
+if [ "$CXX" = "clang++" ]; then cmake -G "Eclipse CDT4 - Unix Makefiles" -DCMAKE_BUILD_TYPE=Debug -DSDL2=OFF -DBUNDLED_FREETYPE=ON ../; fi
